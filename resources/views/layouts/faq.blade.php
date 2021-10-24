be2f856e3c0ff2b619e46db93c391e4d8f18e02f
zahraAfa <anisafatimahazzahra@gmail.com>
2021-10-24 23:01:14 +0700

    Merged with faq

--- resources/views/layouts/faq.blade.php
+++ resources/views/layouts/faq.blade.php
@@ -5,37 +5,21 @@
   <div class="faq-modal-wrapper">
     <i class="close-modal material-icons-round">close</i>
     <div class="faq-container">
-      <h1>FAQ</h1>
+      <h1>FAQ</h1> 
+      {{-- @foreach ($faqs as $faq)
+      <button class="faq-accordion faq-acc-1">
+        {{ var_dump($faq) }}
+        <h3>Q: {{ $faq }}</h3>
+      </button>
+      <div class="faq-panel">
+        <p>A: Around December 2021</p>
+      </div>
+      @endforeach --}}
       <button class="faq-accordion faq-acc-1">
         <h3>Q: When is the mint?</h3>
       </button>
       <div class="faq-panel">
         <p>A: Around December 2021</p>
-      </div>
-      <button class="faq-accordion faq-acc-2">
-        <h3>Q: What is the price of Ordinary Sol?</h3>
-      </button>
-      <div class="faq-panel">
-        <p>A: 1 SOL</p>
-      </div>
-      <button class="faq-accordion faq-acc-3">
-        <h3>Q: What makes Ordinary Sol special?</h3>
-      </button>
-      <div class="faq-panel">
-        <p>A: We are planning for a continual development on the project. The Ordinary concept has a lot of
-          possibilities of development, such as the aging system, marriage, public housing, etc.</p>
-      </div>
-      <button class="faq-accordion faq-acc-4">
-        <h3>Q: What is your sales distribution?</h3>
-      </button>
-      <div class="faq-panel">
-        <p>A: 50% will be in the next phase development, 30% goes to the team, 20% goes to the charity</p>
-      </div>
-      <button class="faq-accordion faq-acc-5">
-        <h3>Q: What are you planning after the drop?</h3>
-      </button>
-      <div class="faq-panel">
-        <p>A: The phase 2, TBD</p>
       </div>
     </div>
     <a href="/" class="telegram-btn">Ask on Telegram</a>
