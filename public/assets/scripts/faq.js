5df464fcb31b1a3976a3e5f3d9fc18983af3e349
zahraAfa <anisafatimahazzahra@gmail.com>
2021-10-25 12:20:36 +0700

    Retrieved FAQ data from api

--- public/assets/scripts/faq.js
+++ public/assets/scripts/faq.js
@@ -2,7 +2,7 @@ var acc = document.getElementsByClassName("faq-accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
+  $(document).on('click','.faq-accordion',function(){
     this.classList.toggle("active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
