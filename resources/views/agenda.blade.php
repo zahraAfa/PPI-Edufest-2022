b7ec054ab019efea0ea89d45c5331c6ddfca7acf
zahraAfa <anisafatimahazzahra@gmail.com>
2021-10-30 21:51:00 +0700

    Agenda body added

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -12,9 +12,27 @@
                 @include('layouts.calendar')
             </div>
             <div class="agenda-body-container">
-                <div class="agenda-filters"></div>
+                <div class="agenda-filters">
+                    <input class="form-control search-bar speakers-search-bar" id="search-input" type="text" placeholder="Search..">
+                    <div class="filter-separator"></div>
+                    <div class="filter-bar speakers-filter-bar dropdown show">
+                        <a class="btn dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Show All
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                            <a class="dropdown-item" href="#">Timur Tengah dan Afrika</a>
+                            <a class="dropdown-item" href="#">Asia dan Oseania</a>
+                            <a class="dropdown-item" href="#">Eropa dan Amerika</a>
+                        </div>
+                    </div>
+                </div>
                 <div class="agenda-grid">
-                    <div class="agenda-card"></div>
+                    <div class="speaker-card" data-aos="fade-up" data-aos-delay="20" data-aos-duration="1000">
+                        <div class="speaker-img-cont">
+                            <img src="assets/img/header/carousel1.jpeg" alt="Speaker">
+                        </div>
+                        <div class="speaker-text-cont"></div>
+                    </div>
                 </div>
             </div>
         </div>
