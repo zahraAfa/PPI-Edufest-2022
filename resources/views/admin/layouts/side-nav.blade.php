cc01d09c2c02727370801d704e4c428b2920d9d3
zahraAfa <anisafatimahazzahra@gmail.com>
2021-11-20 16:52:38 +0700

    routes in side nav edited

--- resources/views/admin/layouts/side-nav.blade.php
+++ resources/views/admin/layouts/side-nav.blade.php
@@ -36,7 +36,7 @@
                 </a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="{{ route('admin-partners-index') }}">
+                <a class="nav-link" href="admin-participants.html">
                     <i class="fa fa-users" style="color: var(--bs-gray-800);"></i>
                     <span style="color: var(--bs-gray-800);">Participants</span>
                 </a>
