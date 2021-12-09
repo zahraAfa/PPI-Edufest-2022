524b02e2c3f326e4e1551c2cc9fb706d8f26bf9c
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-12-09 09:51:35 +0700

    coments unused dom query

--- public/assets/scripts/header.js
+++ public/assets/scripts/header.js
@@ -1,11 +1,11 @@
 const translate = document.querySelectorAll(".translate");
 // const big_title = document.querySelector(".big-title");
 const header = document.querySelector("header");
-const shadow = document.querySelector(".shadow");
+// const shadow = document.querySelector(".shadow");
 // const content = document.querySelector(".content");
 const section = document.querySelector("section");
 // const image_container = document.querySelector(".imgContainer");
-const opacity = document.querySelectorAll(".opacity");
+// const opacity = document.querySelectorAll(".opacity");
 // const border = document.querySelector(".border");
 
 let header_height = header.offsetHeight;
@@ -26,12 +26,12 @@ function update() {
         element.style.transform = `translateY(${latestKnownScrollY * speed}px)`;
     });
 
-    opacity.forEach(element => {
-        element.style.opacity = latestKnownScrollY / (sectionY.top + section_height);
-    })
+    // opacity.forEach(element => {
+    //     element.style.opacity = latestKnownScrollY / (sectionY.top + section_height);
+    // })
 
     // big_title.style.opacity = - latestKnownScrollY / (header_height / 2) + 1;
-    shadow.style.height = `${latestKnownScrollY * 0.5 + 300}px`;
+    // shadow.style.height = `${latestKnownScrollY * 0.5 + 300}px`;
 
     // content.style.transform = `translateY(${latestKnownScrollY / (section_height + sectionY.top) * 50 - 50}px)`;
     // image_container.style.transform = `translateY(${latestKnownScrollY / (section_height + sectionY.top) * -50 + 50}px)`;
