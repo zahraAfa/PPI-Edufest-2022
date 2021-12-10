aa7b800e36d4f7cabdded303d984c79ff289ef39
zahraAfa <anisafatimahazzahra@gmail.com>
2021-12-10 09:50:28 +0700

    media query for agenda page has been implemented

--- resources/views/agenda.blade.php
+++ resources/views/agenda.blade.php
@@ -5,9 +5,13 @@
 <section>
     <div class="agenda-page-container">
         <div class="agenda-page-wrapper">
-            <div class="agenda-page-header">
-                <h1 class="agenda-page-h1">Agenda</h1>
-            </div>
+            <header class="agenda-page-header">
+                <h1 class="agenda-page-h1 translate" data-speed="1">AGENDA</h1>
+                <div class="agenda-scroll-down">
+                    <p>Scroll Down</p>
+                    <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+                </div>
+            </header>
             <div class="calendar-agenda-page-wrapper">
                 <div class="agenda-page-calendar">
                     @include('layouts.calendar')
@@ -16,33 +20,117 @@
                     <div class="filters-div agenda-page-filters">
                         <input class="form-control search-bar agenda-page-search-bar" id="search-input" type="text" placeholder="Search..">
                         <div class="filter-separator"></div>
-                        {{-- <form action="" class="filter-bar"> --}}
                             <select name="Kawasan" id="kawasan-filter" class="filter-bar">
                                 <option value="all">Show all</option>
                                 <option value="timtengka">Timur Tengah dan Afrika</option>
                                 <option value="aseo">Asia dan Oseania</option>
                                 <option value="aerop">Eropa dan Amerika</option>
                             </select>
-                          {{-- </form> --}}
-                        {{-- <div class="filter-bar agenda-page-filter-bar dropdown show">
-                            <a class="btn dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Show All
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                                <a class="dropdown-item" href="#">Timur Tengah dan Afrika</a>
-                                <a class="dropdown-item" href="#">Asia dan Oseania</a>
-                                <a class="dropdown-item" href="#">Eropa dan Amerika</a>
-                            </div>
-                        </div> --}}
                     </div>
-                    <div class="agenda-page-grid" href="/acara">
-                        <a class="agenda-page-card" data-aos="fade-up" data-aos-delay="20" data-aos-duration="1000">
-                            <div class="agenda-page-img-cont">
-                                <img src="assets/img/header/carousel1.jpeg" alt="agenda">
+                    <div class="agenda-page-grid">
+                        <a href="{{ route('usr-event') }}" class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                            <div class="img__card-container">
+                                <img src="assets/img/header/bintang-tamu-special-test.png" class="card__image" alt="Speaker" />
                             </div>
-                            <div class="agenda-page-text-cont">
-                                <h3 class="agenda-page-name-card">Event Name</h3>
-                                <p class="agenda-page-date-card">2021-01-01</p>
+                            <div class="card__overlay">
+                              <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                <div class="card__header-text">
+                                  <h3 class="card__title speaker__name">Event Name</h3>
+                                  <span class="card__status speaker__ppi">2022-01-01</span>
+                                </div>
+                              </div>
+                              <p class="card__description speaker__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                        <a href="{{ route('usr-event') }}" class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                            <div class="img__card-container">
+                                <img src="assets/img/header/bintang-tamu-special-test.png" class="card__image" alt="Speaker" />
+                            </div>
+                            <div class="card__overlay">
+                              <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                <div class="card__header-text">
+                                  <h3 class="card__title speaker__name">Event Name</h3>
+                                  <span class="card__status speaker__ppi">2022-01-01</span>
+                                </div>
+                              </div>
+                              <p class="card__description speaker__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                        <a href="{{ route('usr-event') }}" class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                            <div class="img__card-container">
+                                <img src="assets/img/header/bintang-tamu-special-test.png" class="card__image" alt="Speaker" />
+                            </div>
+                            <div class="card__overlay">
+                              <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                <div class="card__header-text">
+                                  <h3 class="card__title speaker__name">Event Name</h3>
+                                  <span class="card__status speaker__ppi">2022-01-01</span>
+                                </div>
+                              </div>
+                              <p class="card__description speaker__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                        <a href="{{ route('usr-event') }}" class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                            <div class="img__card-container">
+                                <img src="assets/img/header/bintang-tamu-special-test.png" class="card__image" alt="Speaker" />
+                            </div>
+                            <div class="card__overlay">
+                              <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                <div class="card__header-text">
+                                  <h3 class="card__title speaker__name">Event Name</h3>
+                                  <span class="card__status speaker__ppi">2022-01-01</span>
+                                </div>
+                              </div>
+                              <p class="card__description speaker__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                        <a href="{{ route('usr-event') }}" class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                            <div class="img__card-container">
+                                <img src="assets/img/header/bintang-tamu-special-test.png" class="card__image" alt="Speaker" />
+                            </div>
+                            <div class="card__overlay">
+                              <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                <div class="card__header-text">
+                                  <h3 class="card__title speaker__name">Event Name</h3>
+                                  <span class="card__status speaker__ppi">2022-01-01</span>
+                                </div>
+                              </div>
+                              <p class="card__description speaker__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                        <a href="{{ route('usr-event') }}" class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                            <div class="img__card-container">
+                                <img src="assets/img/header/bintang-tamu-special-test.png" class="card__image" alt="Speaker" />
+                            </div>
+                            <div class="card__overlay">
+                              <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                <div class="card__header-text">
+                                  <h3 class="card__title speaker__name">Event Name</h3>
+                                  <span class="card__status speaker__ppi">2022-01-01</span>
+                                </div>
+                              </div>
+                              <p class="card__description speaker__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                            </div>
+                        </a>
+                        <a href="{{ route('usr-event') }}" class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                            <div class="img__card-container">
+                                <img src="assets/img/header/bintang-tamu-special-test.png" class="card__image" alt="Speaker" />
+                            </div>
+                            <div class="card__overlay">
+                              <div class="card__header">
+                                <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                                <div class="card__header-text">
+                                  <h3 class="card__title speaker__name">Event Name</h3>
+                                  <span class="card__status speaker__ppi">2022-01-01</span>
+                                </div>
+                              </div>
+                              <p class="card__description speaker__desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
                             </div>
                         </a>
                     </div>
