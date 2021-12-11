2b242432da4b8f8f216ed11624a700f282369674
Nurizko Maulana <nurizko.maulana@gmail.com>
2021-12-11 15:45:20 +0700

    add delete , update confirmarion and no data mess

--- resources/views/admin/sponsors/index.blade.php
+++ resources/views/admin/sponsors/index.blade.php
@@ -34,9 +34,10 @@
 
                 <div class="container-fluid">
                     <h3 class="text-dark mb-4">Sponsors</h3>
-                    <a class="btn btn-info btn-icon-split" role="button" style="margin-bottom: 15px;" href="{{ route('admin-sponsor-add') }}"><span class="text-white-50 icon"><i
+                    <a class="btn btn-info btn-icon-split" role="button" style="margin-bottom: 15px;"
+                        href="{{ route('admin-sponsor-add') }}"><span class="text-white-50 icon"><i
                                 class="fas fa-plus" style="color: rgb(255,255,255);"></i></span>
-                                <span class="text-white text">New Sponsor</span></a>
+                        <span class="text-white text">New Sponsor</span></a>
                     <div class="card shadow" style="border-radius: 20px;">
                         <div class="card-header py-3"
                             style="border-top-left-radius: 20px;border-top-right-radius: 20px;">
@@ -52,12 +53,14 @@
                                                 <option value="25">25</option>
                                                 <option value="50">50</option>
                                                 <option value="100">100</option>
-                                            </select>&nbsp;</label></div>
+                                            </select>&nbsp;</label>
+                                    </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="text-md-end dataTables_filter" id="dataTable_filter"><label
-                                            class="form-label"><input type="search" class="form-control form-control-sm"
-                                                aria-controls="dataTable" placeholder="Search"></label></div>
+                                            class="form-label"><input type="search"
+                                                class="form-control form-control-sm" aria-controls="dataTable"
+                                                placeholder="Search"></label></div>
                                 </div>
                             </div>
                             <div class="table-responsive table mt-2" id="dataTable" role="grid"
@@ -90,8 +93,8 @@
                                             <li class="page-item active"><a class="page-link" href="#">1</a></li>
                                             <li class="page-item"><a class="page-link" href="#">2</a></li>
                                             <li class="page-item"><a class="page-link" href="#">3</a></li>
-                                            <li class="page-item"><a class="page-link" href="#" aria-label="Next"><span
-                                                        aria-hidden="true">»</span></a></li>
+                                            <li class="page-item"><a class="page-link" href="#"
+                                                    aria-label="Next"><span aria-hidden="true">»</span></a></li>
                                         </ul>
                                     </nav>
                                 </div>
@@ -107,55 +110,102 @@
             </footer>
         </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure want to delete this sponsor?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancle</button>
+                    <button type="button" id="modal_delete" class="btn btn-primary">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="../../assets/admin-template/bootstrap/js/bootstrap.min.js"></script>
     <script src="../../assets/admin-template/js/script.min.js"></script>
     <script>
-        $(document).ready( function(){
+        // get modal element
+        var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+            keyboard: false
+        })
+
+        $(document).ready(function() {
             $.ajax({
                 type: "GET",
                 url: "../../../api/sponsors/read",
-                header:{
+                header: {
                     "Authorization": "Bearer {{ Auth::user()->api_token }}"
                 },
-                success: function (result) {
+                success: function(result) {
 
-                var sponsorItems ='';
-                $.each(result, function(key, sponsor){
-                    sponsorItems += '<tr>'+'<td><img class="rounded-circle me-2" width="150" height="150" src="../../storage/img/sponsors/'+sponsor["id"]+'/'+sponsor["picture"]+'"></td>'+
-                                        '<td id="sponsor__name-crud">'+sponsor["name"]+'</td>'+
-                                        '<td id="sponsor__detail-crud">'+sponsor["detail"]+'</td>'+
-                                        '<td>'+
-                                            '<a style="background-color:#1cc88a!important;" class="btn btn-success btn-circle ms-1" role="button" id="sponsor__edit" href="../../admin/sponsors/edit/'+sponsor["id"]+'"><i class="fas fa-edit text-white"></i></a>'+
-                                            '<a style="background-color:#e74a3b!important;" class="btn btn-danger btn-circle ms-1 delete-btn-sponsor" role="button" id="'+sponsor["id"]+'"><i class="fas fa-trash text-white"></i></a>'+
-                                        '</td>'+
+                    console.log(result);
+
+                    if (result.length === 0) {
+                        $('#sponsors__row').after('<td colspan="4" class="text-center" >There is no data exist here</td>');
+                    } else {
+                        var sponsorItems = '';
+                        $.each(result, function(key, sponsor) {
+                            sponsorItems += '<tr>' +
+                                '<td><img class="rounded-circle me-2" width="150" height="150" src="../../storage/img/sponsors/' +
+                                sponsor["id"] + '/' + sponsor["picture"] + '"></td>' +
+                                '<td id="sponsor__name-crud">' + sponsor["name"] + '</td>' +
+                                '<td id="sponsor__detail-crud">' + sponsor["detail"] + '</td>' +
+                                '<td>' +
+                                '<a style="background-color:#1cc88a!important;" class="btn btn-success btn-circle ms-1" role="button" id="sponsor__edit" href="../../admin/sponsors/edit/' +
+                                sponsor["id"] + '"><i class="fas fa-edit text-white"></i></a>' +
+                                '<a style="background-color:#e74a3b!important;" class="btn btn-danger btn-circle ms-1 delete-btn-sponsor" role="button" id="' +
+                                sponsor["id"] +
+                                '"><i class="fas fa-trash text-white"></i></a>' +
+                                '</td>' +
                                 '</tr>';
-                });
-                $('#sponsors__row').after(sponsorItems);
+                        });
+                        $('#sponsors__row').after(sponsorItems);
+
+                    }
                 }
             });
         });
-        $(document).on('click',".delete-btn-sponsor",function() {
+        $(document).on('click', ".delete-btn-sponsor", function() {
 
-                var del_id = $(this).attr('id');
-                $.ajax({
-                    type: "DELETE",
-                    url: '../../../api/sponsors/delete/'+del_id,
-                    data: 'id='+del_id ,
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader('Authorization', 'Bearer {{ Auth::user()->api_token }}');
-                    },
-                    success: function(data)
-                    {
-                        alert(data["response"]);
-                        location.reload();
-                    },
-                    error: function(XMLHttpRequest, textStatus, errorThrown) {
-                        var data=XMLHttpRequest.responseText;
-                        var jsonResponse = JSON.parse(data);
-                        alert(jsonResponse["message"]);
-                    }
-                });
+            var del_id = $(this).attr('id');
+
+            myModal.show();
+
+            $("#modal_delete").attr("delete-id", del_id);
+
+
+        });
+
+        $(document).on('click', "#modal_delete", function() {
+            let del_id = $(this).attr('delete-id');
+
+            $.ajax({
+                type: "DELETE",
+                url: '../../../api/sponsors/delete/' + del_id,
+                data: 'id=' + del_id,
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader('Authorization', 'Bearer {{ Auth::user()->api_token }}');
+                },
+                success: function(data) {
+                    location.reload();
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    var data = XMLHttpRequest.responseText;
+                    var jsonResponse = JSON.parse(data);
+                    alert(jsonResponse["message"]);
+                }
             });
+
+            myModal.hide();
+
+        });
     </script>
 </body>
 
