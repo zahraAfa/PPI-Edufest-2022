fb7f0f5b2b5a754ffd9a3d5a39b14d194b3a4c66
zahraAfa <anisafatimahazzahra@gmail.com>
2022-01-12 13:02:15 +0700

    articles added to the main page

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -4,28 +4,129 @@
 
 <section class="articles-section">
     <div class="articles-main-header">
-        <h1>"Artikel"</h1>
+        <div class="articles-main-h1">
+            <h1>"Artikel."</h1>
+        </div>
+        <div class="articles-main-header-deco">
+            <h1>.22</h1>
+        </div>
+        <div class="articles-header-background">
+            <img class="articles-header-bg1" src="../../../../../../assets/img/header/articles-deco.png" alt="">
+            <img class="articles-header-bg2" src="../../../../../../assets/img/header/articles-deco-back.png" alt="">
+        </div>
     </div>
     <div class="articles-main-content">
         <div class="articles-main-content-wrapper">
             <div class="articles-box">
-                <div class="articles-box-pic"></div>
-                <div class="articles-box-content"></div>
+                <div class="articles-box-row">
+                    <h1 class="articles-box-title">Membaca Peluang dan Tantangan Kehidupan Luar Negeri bagi Generasi Muda Indonesia</h1>
+                    <h2 class="articles-box-subtitle">Nurul Khair</h2>
+                </div>
+                <div class="articles-box-row">
+                    <p class="articles-box-desc">
+                        “Kalau pemuda berusia 21-22 tahun sama sekali tidak berjuang, tak bercita-
+                        cita, tak bergiat untuk tanah air dan bangsa, pemuda begini baiknya
+                        digunduli saja kepalanya” Ir. Seokarno
+                    </p>
+                </div>
+                <div class="articles-box-row">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#articlePdfModal" class="btn button-56">Baca</button>
+                </div>
             </div>
             <div class="articles-box">
-                <div class="articles-box-pic"></div>
-                <div class="articles-box-content"></div>
+                <div class="articles-box-row">
+                    <h1 class="articles-box-title">Membaca Peluang dan Tantangan Kehidupan Luar Negeri bagi Generasi Muda Indonesia</h1>
+                    <h2 class="articles-box-subtitle">Nurul Khair</h2>
+                </div>
+                <div class="articles-box-row">
+                    <p class="articles-box-desc">
+                        “Kalau pemuda berusia 21-22 tahun sama sekali tidak berjuang, tak bercita-
+                        cita, tak bergiat untuk tanah air dan bangsa, pemuda begini baiknya
+                        digunduli saja kepalanya” Ir. Seokarno
+                    </p>
+                </div>
+                <div class="articles-box-row">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#articlePdfModal" class="btn button-56">Baca</button>
+                </div>
             </div>
             <div class="articles-box">
-                <div class="articles-box-pic"></div>
-                <div class="articles-box-content"></div>
+                <div class="articles-box-row">
+                    <h1 class="articles-box-title">Membaca Peluang dan Tantangan Kehidupan Luar Negeri bagi Generasi Muda Indonesia</h1>
+                    <h2 class="articles-box-subtitle">Nurul Khair</h2>
+                </div>
+                <div class="articles-box-row">
+                    <p class="articles-box-desc">
+                        “Kalau pemuda berusia 21-22 tahun sama sekali tidak berjuang, tak bercita-
+                        cita, tak bergiat untuk tanah air dan bangsa, pemuda begini baiknya
+                        digunduli saja kepalanya” Ir. Seokarno
+                    </p>
+                </div>
+                <div class="articles-box-row">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#articlePdfModal" class="btn button-56">Baca</button>
+                </div>
             </div>
             <div class="articles-box">
-                <div class="articles-box-pic"></div>
-                <div class="articles-box-content"></div>
+                <div class="articles-box-row">
+                    <h1 class="articles-box-title">Membaca Peluang dan Tantangan Kehidupan Luar Negeri bagi Generasi Muda Indonesia</h1>
+                    <h2 class="articles-box-subtitle">Nurul Khair</h2>
+                </div>
+                <div class="articles-box-row">
+                    <p class="articles-box-desc">
+                        “Kalau pemuda berusia 21-22 tahun sama sekali tidak berjuang, tak bercita-
+                        cita, tak bergiat untuk tanah air dan bangsa, pemuda begini baiknya
+                        digunduli saja kepalanya” Ir. Seokarno
+                    </p>
+                </div>
+                <div class="articles-box-row">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#articlePdfModal" class="btn button-56">Baca</button>
+                </div>
+            </div>
+            <div class="articles-box">
+                <div class="articles-box-row">
+                    <h1 class="articles-box-title">Membaca Peluang dan Tantangan Kehidupan Luar Negeri bagi Generasi Muda Indonesia</h1>
+                    <h2 class="articles-box-subtitle">Nurul Khair</h2>
+                </div>
+                <div class="articles-box-row">
+                    <p class="articles-box-desc">
+                        “Kalau pemuda berusia 21-22 tahun sama sekali tidak berjuang, tak bercita-
+                        cita, tak bergiat untuk tanah air dan bangsa, pemuda begini baiknya
+                        digunduli saja kepalanya” Ir. Seokarno
+                    </p>
+                </div>
+                <div class="articles-box-row">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#articlePdfModal" class="btn button-56">Baca</button>
+                </div>
+            </div>
+            <div class="articles-box">
+                <div class="articles-box-row">
+                    <h1 class="articles-box-title">Membaca Peluang dan Tantangan Kehidupan Luar Negeri bagi Generasi Muda Indonesia</h1>
+                    <h2 class="articles-box-subtitle">Nurul Khair</h2>
+                </div>
+                <div class="articles-box-row">
+                    <p class="articles-box-desc">
+                        “Kalau pemuda berusia 21-22 tahun sama sekali tidak berjuang, tak bercita-
+                        cita, tak bergiat untuk tanah air dan bangsa, pemuda begini baiknya
+                        digunduli saja kepalanya” Ir. Seokarno
+                    </p>
+                </div>
+                <div class="articles-box-row">
+                    <button type="button" data-bs-toggle="modal" data-bs-target="#articlePdfModal" class="btn button-56">Baca</button>
+                </div>
             </div>
         </div>
     </div>
 </section>
+<div class="modal fade" id="articlePdfModal" tabindex="-1" aria-labelledby="articlePdfModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body articles-modal-body">
+            <iframe class="mx-auto" src="https://drive.google.com/file/d/1z6tiBJtsczVlEcLYKPRaDlCWsxSBJXQl/preview" allow="autoplay"></iframe>
+        </div>
+      </div>
+    </div>
+</div>
 
 @endsection
