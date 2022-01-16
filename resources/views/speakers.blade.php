9dc075625588092e2d39016fd14df9049b419375
Nurizko Maulana <nurizko.maulana@gmail.com>
2022-01-17 01:48:37 +0700

    add search by name & region

--- resources/views/speakers.blade.php
+++ resources/views/speakers.blade.php
@@ -28,8 +28,10 @@
                 </div>
                 <div class="speakers-content">
                     <div class="filters-div">
-                        <input class="form-control search-bar speakers-search-bar" id="search-input" type="text"
-                            placeholder="Search..">
+                        <form id="form_speaker" class="m-0">
+                            <input class="form-control search-bar speakers-search-bar" id="search-input" type="text"
+                                placeholder="Search..">
+                        </form>
                         <div class="filter-separator"></div>
                         <select name="Kawasan" id="kawasan-filter" class="filter-bar">
                             <option value="all">Show all</option>
@@ -48,6 +50,14 @@
         </div>
     </section>
     <script>
+        $("#form_speaker").submit(function(e) {
+            e.preventDefault();
+            searchSpeakerByName();
+        });
+        $('#kawasan-filter').on('change', function() {
+            console.log("oke serach")
+            searchSpeakerByRegion();
+        })
         $(document).ready(function() {
             $.ajax({
                 type: "GET",
@@ -96,6 +106,86 @@
                     });
                 }
             });
+
         });
+
+        function injectData(data, pagination) {
+            var listSpeaker = '';
+            if (data.length == 0) {
+                listSpeaker = '<h3 class="text-white">Tidak ada hasil. </h3>'
+            }
+            $.each(data, function(key, speaker) {
+                listSpeaker +=
+                    `
+                              <div class="custom-card" data-aos="fade-up" data-aos-delay="30" data-aos-duration="2000">
+                              <div class="img__card-container">
+                                <img src="../../storage/img/speakers/${speaker["id"]}/${speaker["picture"]}" class="card__image" alt="Speaker" />
+                              </div>
+                              <div class="card__overlay">
+                                <div class="card__header">
+                                    <svg class="card__arc" xmlns="http://www.w3.org/2000/svg">
+                                        <path />
+                                    </svg>
+                                    <div class="card__header-text">
+                                        <h3 class="card__title speaker__name">${speaker['name']}</h3>
+                                        <span class="card__status speaker__ppi"><i
+                                                class="material-icons-round">public</i>${speaker['ppi']}</span>
+                                    </div>
+                                </div>
+                                <p class="card__description speaker__desc">${speaker['detail']}</p>
+                              </div>
+                              </div>
+                            `
+            });
+            $('.speakers-grid').html(listSpeaker);
+            $(".paginationjs-pages").click(function() {
+                $([document.documentElement, document.body])
+                    .animate({
+                        scrollTop: $(".speakers-grid")
+                            .offset().top
+                    }, 100);
+            });
+        }
+
+        function searchSpeakerByName() {
+            console.log("oke search")
+            let searchString = $("#search-input").val();
+            $.ajax({
+                type: "GET",
+                url: `../../../api/speakers/read?search=${searchString}`,
+                className: 'paginationjs-theme-red',
+                success: function(result) {
+                    let container = $('.paginate');
+                    container.pagination({
+                        dataSource: result,
+                        pageSize: 12,
+                        className: 'paginationjs-theme-red',
+                        callback: function(data, pagination) {
+                            injectData(data, pagination);
+                        }
+                    });
+                }
+            });
+        }
+
+        function searchSpeakerByRegion() {
+            let searchString = $("#kawasan-filter").val();
+            $.ajax({
+                type: "GET",
+                url: `../../../api/speakers/read?region=${searchString}`,
+                className: 'paginationjs-theme-red',
+                success: function(result) {
+                    let container = $('.paginate');
+                    container.pagination({
+                        dataSource: result,
+                        pageSize: 12,
+                        className: 'paginationjs-theme-red',
+                        callback: function(data, pagination) {
+                            injectData(data, pagination);
+                        }
+                    });
+                }
+            });
+        }
     </script>
 @endsection
