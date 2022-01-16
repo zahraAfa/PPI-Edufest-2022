901416da6023288fd84e801b750bd2660064fda6
Nurizko Maulana <nurizko.maulana@gmail.com>
2022-01-16 19:05:59 +0700

    add analytic

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     {{-- <link rel="stylesheet" type="text/css" href="slick/slick.css" />
     <link rel="stylesheet" type="text/css" href="slick/slick-theme.css" /> --}}
     <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
@@ -35,6 +36,19 @@
     <!-- Favicon start -->
     <link rel="icon" type="image/png" sizes="16x16" href="assets/icons/edufest-icon.png">
     <!-- Favicon end -->
+
+    <!-- START Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BFQS6Z9RBS"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'G-BFQS6Z9RBS');
+    </script>
+     <!-- END Global site tag (gtag.js) - Google Analytics -->
+
 </head>
 
 <body class="main-page" id="top-page">
@@ -46,14 +60,19 @@
             <div class="nav-body">
                 <div class="nav-sections">
                     <ul class="nav-items">
-                        <li><a href="{{ ($title === "Home")?'#': route('usr-home') }}" class="nav-beranda">Beranda</a></li>
-                        <li><a href="{{ ($title === "Pembicara")?'#': route('usr-speaker') }}" class="nav-pembicara">Pembicara</a></li>
-                        <li><a href="{{ ($title === "Agenda")?'#': route('usr-agenda') }}" class="nav-agenda">Agenda</a></li>
+                        <li><a href="{{ $title === 'Home' ? '#' : route('usr-home') }}"
+                                class="nav-beranda">Beranda</a></li>
+                        <li><a href="{{ $title === 'Pembicara' ? '#' : route('usr-speaker') }}"
+                                class="nav-pembicara">Pembicara</a></li>
+                        <li><a href="{{ $title === 'Agenda' ? '#' : route('usr-agenda') }}"
+                                class="nav-agenda">Agenda</a></li>
                         <li><a href="#" class="nav-writingcont">Writing Contest</a></li>
-                        <li><a href="{{ ($title === "Docs")?'#': route('usr-docs')}}" class="nav-dokumentasi">Dokumentasi</a></li>
+                        <li><a href="{{ $title === 'Docs' ? '#' : route('usr-docs') }}"
+                                class="nav-dokumentasi">Dokumentasi</a></li>
                     </ul>
                     <div class="title-logo">
-                        <a href="{{ ($title === "Home")?'#': route('usr-home') }}" class="gohome"><img src="assets/img/logos/Logo-Edufest.png" alt="Edufest Logo"></a>
+                        <a href="{{ $title === 'Home' ? '#' : route('usr-home') }}" class="gohome"><img
+                                src="assets/img/logos/Logo-Edufest.png" alt="Edufest Logo"></a>
                     </div>
                     <div class="nav-wrap">
                         <div class="menu-toggle">
@@ -87,7 +106,9 @@
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     {{-- <script type="text/javascript" src="slick/slick.min.js"></script> --}}
     <script src="assets/scripts/slick-slider.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
