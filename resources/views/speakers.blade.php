a47a577dbd4bbefe8a10b5903ea02d331ade1e84
Nurizko Maulana <nurizko.maulana@gmail.com>
2022-01-18 19:59:29 +0700

    fix filter region

--- resources/views/speakers.blade.php
+++ resources/views/speakers.blade.php
@@ -34,10 +34,11 @@
                         </form>
                         <div class="filter-separator"></div>
                         <select name="Kawasan" id="kawasan-filter" class="filter-bar">
-                            <option value="all">Show all</option>
+                            <option value="">Show all</option>
+                            <option value="all_region">All Region</option>
                             <option value="timtengka">Timur Tengah dan Afrika</option>
-                            <option value="aseo">Asia dan Oseania</option>
-                            <option value="aerop">Eropa dan Amerika</option>
+                            <option value="asia_oseania">Asia dan Oseania</option>
+                            <option value="amerop">Eropa dan Amerika</option>
                         </select>
                     </div>
                     <div class="speakers-grid">
