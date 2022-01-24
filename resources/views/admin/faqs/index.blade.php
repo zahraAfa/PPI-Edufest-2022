a9384b045f76b74919ede193dc039f716d1ec0b3
Nurizko Maulana <nurizko.maulana@gmail.com>
2022-01-24 19:42:40 +0700

    add delete confirmation

--- resources/views/admin/faqs/index.blade.php
+++ resources/views/admin/faqs/index.blade.php
@@ -40,15 +40,18 @@
 
                 <div class="container-fluid">
                     <h3 class="text-dark mb-4">Frequently Asked Questions&nbsp;</h3>
-                    <a class="btn btn-info btn-icon-split" role="button" id="faq__add-crud" style="margin-bottom: 13px;margin-top: 0px;" href="{{ route('admin-faq-add') }}">
+                    <a class="btn btn-info btn-icon-split" role="button" id="faq__add-crud"
+                        style="margin-bottom: 13px;margin-top: 0px;" href="{{ route('admin-faq-add') }}">
                         <span class="text-white-50 icon">
                             <i class="fas fa-plus" style="color: rgb(255,255,255);"></i>
                         </span>
                         <span class="text-white text">New FAQ</span>
                     </a>
                     <div class="card shadow" style="border-radius: 20px;">
-                        <div class="card-header py-3" style="border-top-left-radius: 20px;border-top-right-radius: 20px;">
-                            <p class="text-primary m-0 fw-bold" style="border-color: rgb(78, 115, 223);border-top-color: rgb(78,;border-right-color: 115,;border-bottom-color: 223);border-left-color: 115,;">
+                        <div class="card-header py-3"
+                            style="border-top-left-radius: 20px;border-top-right-radius: 20px;">
+                            <p class="text-primary m-0 fw-bold"
+                                style="border-color: rgb(78, 115, 223);border-top-color: rgb(78,;border-right-color: 115,;border-bottom-color: 223);border-left-color: 115,;">
                                 FAQ List
                             </p>
                         </div>
@@ -77,57 +80,87 @@
             </footer>
         </div><a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Delete</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Are you sure want to delete this event?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" id="modal_delete" class="btn btn-primary">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="../assets/admin-template/bootstrap/js/bootstrap.min.js"></script>
     <script src="../assets/admin-template/js/script.min.js"></script>
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.js"></script>
     <script>
-        $(document).ready( function(){
+        // get modal element
+        var myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+            keyboard: false
+        });
+        $(document).ready(function() {
             $.ajax({
                 type: "GET",
                 url: "../../api/faqs/read",
-                header:{
+                header: {
                     "Authorization": "Bearer {{ Auth::user()->api_token }}"
                 },
-                success: function (result) {
+                success: function(result) {
 
-                var faqItems ='';
-                $.each(result, function(key, faq){
+                    var faqItems = '';
+                    $.each(result, function(key, faq) {
 
-                    faqItems += '<tr>'+'<td id="faq__question-crud">'+faq["question"]+'</td>'+
-                                        '<td id="faq__answer-crud">'+faq["answer"]+'</td>'+
-                                        '<td><a style="background-color:#1cc88a!important;" class="btn btn-success btn-circle ms-1" role="button" id="faq__edit" href="../admin/faqs/edit/'+faq["id"]+'"><i class="fas fa-edit text-white"></i></a>'+
-                                            '<a style="background-color:#e74a3b!important;" class="btn btn-danger btn-circle ms-1 delete-btn-faq" role="button" id="'+faq["id"]+'"><i class="fas fa-trash text-white"></i></a>'+
-                                        '</td>'+
-                                '</tr>';
-                });
-                $('#faq__row').append(faqItems);
-                $('#dataTable').DataTable();
+                        faqItems += '<tr>' + '<td id="faq__question-crud">' + faq["question"] +
+                            '</td>' +
+                            '<td id="faq__answer-crud">' + faq["answer"] + '</td>' +
+                            '<td><a style="background-color:#1cc88a!important;" class="btn btn-success btn-circle ms-1" role="button" id="faq__edit" href="../admin/faqs/edit/' +
+                            faq["id"] + '"><i class="fas fa-edit text-white"></i></a>' +
+                            '<a style="background-color:#e74a3b!important;" class="btn btn-danger btn-circle ms-1 delete-btn-faq" role="button" id="' +
+                            faq["id"] + '"><i class="fas fa-trash text-white"></i></a>' +
+                            '</td>' +
+                            '</tr>';
+                    });
+                    $('#faq__row').append(faqItems);
+                    $('#dataTable').DataTable();
                 }
             });
         });
 
-            $(document).on('click',".delete-btn-faq",function() {
+        $(document).on('click', ".delete-btn-faq", function() {
+            var del_id = $(this).attr('id');
+            myModal.show();
+            $("#modal_delete").attr("delete-id", del_id);
+        });
 
-                var del_id = $(this).attr('id');
-                $.ajax({
-                    type: "DELETE",
-                    url: '../../../api/faqs/delete/'+del_id,
-                    data: 'id='+del_id ,
-                    beforeSend: function (xhr) {
-                        xhr.setRequestHeader('Authorization', 'Bearer {{ Auth::user()->api_token }}');
-                    },
-                    success: function(data)
-                    {
-                        alert(data["response"]);
-                        location.reload();
-                    },
-                    error: function(XMLHttpRequest, textStatus, errorThrown) {
-                        var data=XMLHttpRequest.responseText;
-                        var jsonResponse = JSON.parse(data);
-                        alert(jsonResponse["message"]);
-                    }
-                });
+        $(document).on('click', "#modal_delete", function() {
+            let del_id = $(this).attr('delete-id');
+
+            $.ajax({
+                type: "DELETE",
+                url: '../../../api/faqs/delete/' + del_id,
+                data: 'id=' + del_id,
+                beforeSend: function(xhr) {
+                    xhr.setRequestHeader('Authorization', 'Bearer {{ Auth::user()->api_token }}');
+                },
+                success: function(data) {
+                    location.reload();
+                },
+                error: function(XMLHttpRequest, textStatus, errorThrown) {
+                    var data = XMLHttpRequest.responseText;
+                    var jsonResponse = JSON.parse(data);
+                    alert(jsonResponse["message"]);
+                }
             });
+            myModal.hide();
+        });
     </script>
 </body>
 
