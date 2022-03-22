43a947d226baccd48667e84049bd1e50e8c724d2
zahraAfa <anisafatimahazzahra@gmail.com>
2022-03-22 18:04:44 +0700

    omega edu user side page has been created

--- resources/views/omegaedu/index.blade.php
+++ resources/views/omegaedu/index.blade.php
@@ -5,15 +5,44 @@
 <section>
     <header class="omega-index-header">
         <h1 class="omega-index-title translate" data-speed="0.6">OMEGA EDU</h1>
-        {{-- <div class="omega-index-header-img">
-            <img src="../../assets/img/header/pencil.png" class="omega-index-pencil" alt="">
+        <div class="omega-index-header-img">
+            <img src="../../assets/img/header/main-header-omegaedu.png" class="omega-index-book" alt="">
             <div class="omega-halfcircle translate" data-speed="0.6"></div>
         </div>
-        <img class="omega-curve-r translateX" data-speed="0.6" src="../../assets/img/header/header-curve-right.png" alt="">
-        <img class="omega-curve-l translateX" data-speed="-0.6" src="../../assets/img/header/header-curve-left.png" alt=""> --}}
     </header>
     <div class="omega-index-body">
-
+        <div class="omega-lists">
+            <a class="omega-list-card" >
+                <div class="campus-logo-wrapper">
+                    <img src="../../../assets/img/omegaedu/logo2.png" alt="univ logo">
+                </div>
+                <h3 class="campus-name">Stanford</h3>
+            </a>
+            <a class="omega-list-card" >
+                <div class="campus-logo-wrapper">
+                    <img src="../../../assets/img/omegaedu/logo3.png" alt="univ logo">
+                </div>
+                <h3 class="campus-name">Stanforda sjajs asjdpaasjdiajsidjai</h3>
+            </a>
+            <a class="omega-list-card" >
+                <div class="campus-logo-wrapper">
+                    <img src="../../../assets/img/omegaedu/logo3.png" alt="univ logo">
+                </div>
+                <h3 class="campus-name">Stanford</h3>
+            </a>
+            <a class="omega-list-card" >
+                <div class="campus-logo-wrapper">
+                    <img src="../../../assets/img/omegaedu/logo2.png" alt="univ logo">
+                </div>
+                <h3 class="campus-name">Stanford</h3>
+            </a>
+            <a class="omega-list-card" >
+                <div class="campus-logo-wrapper">
+                    <img src="../../../assets/img/omegaedu/logo2.png" alt="univ logo">
+                </div>
+                <h3 class="campus-name">Stanford</h3>
+            </a>
+        </div>
 
     </div>
 </section>
